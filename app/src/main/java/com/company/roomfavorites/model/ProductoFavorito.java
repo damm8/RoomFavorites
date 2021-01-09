88f8607ac9b5de426d8e41ddf6972b2d2e82eb00
gerardfp <gerardfp@gmail.com>
2021-01-09 01:29:49 +0100

    Initial commit

--- app/src/main/java/com/company/roomfavorites/model/ProductoFavorito.java
+++ app/src/main/java/com/company/roomfavorites/model/ProductoFavorito.java
@@ -3,7 +3,10 @@ package com.company.roomfavorites.model;
 
 /* Esta clase es para almacenar el resultado de la consulta
         "SELECT Producto.id, Producto.nombre, CASE WHEN userId IS NOT NULL THEN 1 ELSE 0 END as esFavorito FROM Producto LEFT JOIN (SELECT * FROM Favorito WHERE userId = :userId) AS Fav ON Producto.id = Fav.productoId"
- */
+
+    Es decir, toda la lista de productos con un campo extra "esFavorito", que indica para cada Producto
+    si es favorito para un usuario determinado (:userId)
+*/
 
 public class ProductoFavorito {
     public int id;
